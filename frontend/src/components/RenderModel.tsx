62fff6391e30b9343758f8634ccbb62d0400aa9b
John Li (Tet) <tet@tet.moe>
2023-01-22 05:03:18 -0800

    vtubing part partially works

---
+++ frontend/src/components/RenderModel.tsx
@@ -0,0 +1,36 @@
+import { VRM } from "@pixiv/three-vrm";
+import { Canvas } from "@react-three/fiber";
+import React, { useRef } from "react";
+import * as THREE from "three";
+import { PerspectiveCamera as PCamera, OrbitControls } from "@react-three/drei";
+
+export const RenderModel = (props: { model: VRM }) => {
+  const { model } = props;
+
+  return (
+    <Canvas
+      // eventSource={globalThis?.document?.getElementById("root")!}
+      // eventPrefix="client"
+      shadows={true}
+      className={`bg-gray-500 w-full h-full absolute top-0 left-0`}
+      frameloop="demand"
+    >
+      <PCamera
+        // position={new Vector3(-1, 0, 4)}
+        makeDefault
+        zoom={1}
+        ref={camera}
+      ></PCamera>
+      {/* <CLog /> */}
+
+      <OrbitControls camera={camera.current} />
+      <group>
+        <primitive object={model.scene} />
+      </group>
+      {/* <mesh> 
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color={"orange"} />
+        </mesh>  */}
+    </Canvas>
+  );
+};
